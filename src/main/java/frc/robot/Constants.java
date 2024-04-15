21696d6805183a2b2dbc3b9dd444b384f55b7bd5
Jake <114447363+V0KAL@users.noreply.github.com>
2024-04-15 19:51:19 -0400

    Updated Constants
    No magic numbers yet

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
 public class Constants {
+    public class SwerveDrive {
+        public final double chassisLength = 0; // The length of the Chassis
+        public final double chassisWidth = 0; // The Width if the Chassis
 
+    }
 }
