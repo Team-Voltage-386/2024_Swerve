1a77b53d9b97a0d9ed97759d0cd45e7a1e0c6028
Alyssa <alyssa.condon@franklinsabers.org>
2025-01-09 20:38:51 -0500

    Imports

--- src/main/java/frc/robot/Subsystems/FollowTag.java
+++ src/main/java/frc/robot/Subsystems/FollowTag.java
@@ -1,6 +1,18 @@
 package frc.robot.Subsystems;
 
 import frc.robot.Utils.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagPoseEstimator;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.HttpCamera;
+import edu.wpi.first.cscore.VideoSink;
+import edu.wpi.first.cscore.HttpCamera.HttpCameraKind;
+
 public class FollowTag {
     
 }
